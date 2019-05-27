44623d9e1d5ff1eb001e2304af9a87122fd7e77c
Heiker Curiel <heikercuriel@gmail.com>
2019-05-27 08:54:06 -0400

    parse args in chunks
    look mom, no ifs
    
    simplify filename getter

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::env;
 use std::io::Error;
 use std::process::{Child, Command};
 
-struct Notification<'a> {
-    title: &'a str,
+struct Notification {
+    title: String,
     body: String,
 }
 
-impl<'a> Notification<'a> {
-    fn new(args: &HashMap<String, String>) -> Notification {
+impl Notification {
+    fn new(args: HashMap<String, String>) -> Notification {
         fn not_empty(val: &&String) -> bool {
             !val.is_empty()
         }
@@ -18,7 +18,7 @@ impl<'a> Notification<'a> {
             args.get("file")
                 .filter(not_empty)
                 .map(|str| match str.rfind('/') {
-                    Some(position) => &str.split_at(position).1[1..],
+                    Some(position) => str.split_at(position + 1).1,
                     None => str.as_str(),
                 })
         };
@@ -41,7 +41,7 @@ impl<'a> Notification<'a> {
             .fold(artist.unwrap(), |acc, val| format!("{}\n{}", acc, val));
 
         Notification {
-            title: title.or_else(file).unwrap_or("Unknown song"),
+            title: title.or_else(file).unwrap_or("Unknown song").to_string(),
             body: message,
         }
     }
@@ -54,29 +54,23 @@ impl<'a> Notification<'a> {
     }
 }
 
-fn parse_args(args: &[String]) -> HashMap<String, String> {
+fn parse_args(args: &Vec<String>) -> HashMap<String, String> {
     let mut result = HashMap::new();
-    let range = 3..args.len();
     let empty = String::from("");
 
-    for index in range.step_by(2) {
-        let arg_val = args.get(index + 1).unwrap_or(&empty).to_string();
-        result.insert(args[index].to_string(), arg_val);
+    for chunk in args.chunks(2) {
+        let val = chunk.get(1).unwrap_or(&empty);
+        result.insert(chunk[0].to_string(), val.to_string());
     }
 
     result
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().skip(1).collect();
+    let run = |_| Notification::new(parse_args(&args)).show().ok();
 
-    let is_playing = args
-        .get(1..3)
+    args.get(0..2)
         .filter(|val| val == &["status", "playing"])
-        .is_some();
-
-    if is_playing {
-        let args = parse_args(&args);
-        Notification::new(&args).show().ok();
-    }
+        .and_then(run);
 }
